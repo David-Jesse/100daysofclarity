4cfd9e3fd2ecd526479489a75387ee0e7c59a918
David-Jesse <djesse351@gmail.com>
2023-04-02 19:04:50 -0700

    Intro to automated tests

--- tests/offspring-will_test.ts
+++ tests/offspring-will_test.ts
@@ -1,4 +1,4 @@
-
+// https://deno.land/x/clarinet@v1.4.0/index.ts
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.4.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
 
@@ -23,4 +23,18 @@ Clarinet.test({
         assertEquals(block.receipts.length, 0);
         assertEquals(block.height, 3);
     },
+});
+
+Clarinet.test({
+    name: "Get non-existent offspring-wallet, return none",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+        // All function to test
+        let deploy = accounts.get("deployer")!;
+        let wallet_1 = accounts.get("wallet_1")!;
+        
+        let readResult = chain.callReadOnlyFn("offspring-will", "get-offspring-wallet", [types.principal(wallet_1.address)], deploy.address)
+
+        readResult.result.expectOk();
+    }
 });
