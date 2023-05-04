1f593d12899effd14b0590107c790700d2d3711c
David-Jesse <djesse351@gmail.com>
2023-05-04 04:43:36 -0700

    made changes to nft-simple test

--- tests/nft-simple_test.ts
+++ tests/nft-simple_test.ts
@@ -2,6 +2,29 @@
 import { Clarinet, Tx, Chain, Account, Contract, types } from 'https://deno.land/x/clarinet@v1.4.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
 
+// Clarinet.test({
+//     name: "Ensure that the right amount of STX (nft-price ) is transfered by checking balance ",
+//     async fn(chain: Chain, accounts: Map<string, Account>, contracts: Map<string, Contract>) {
+//         let deployer = accounts.get("deployer")!;
+//         let wallet_1 = accounts.get("wallet_1")!;
+
+//         let block = chain.mineBlock([
+//             Tx.contractCall("nft-simple", "mint", [], wallet_1.address)
+//         ]);
+
+//         block.receipts[0].result.expectOk().expectBool(true);
+        
+//         console.log(block.receipts[0].eevents)
+        
+//         block.receipts[0].events.expectSTXTransferEvent(
+//              10000000,
+//              wallet_1.address,
+//              deployer.address
+//         )
+
+//     }
+// })
+
 Clarinet.test({
     name: "Ensure that the right NFT is minted to the right address",
     async fn(chain: Chain, accounts: Map<string, Account>, contracts: Map<string, Contract>) {
